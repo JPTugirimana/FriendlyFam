628840331a266fd97d247e349e338b4b30a8071d
JP Tugirimana <tugirimana.jp@gmail.com>
2021-01-14 14:54:57 +0200

    fixed the bug under if __name__ (app.run())

--- app.py
+++ app.py
@@ -163,10 +163,4 @@ def logout():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    import os
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+    app.run()
